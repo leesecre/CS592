fae00b7fdc210b212db57fd80338ecdb291086a6
Insun Baek <insunbaek@Insuns-MacBook-Air.local>
2023-11-27 14:48:39 +0900

    Initial commit

---
+++ query.pl
@@ -0,0 +1,29 @@
+
+
+logical_expression_to_list(egg(Atom), ['egg']).
+
+
+logical_expression_to_list(Term, [Predicate]) :-
+    Term = ..[Predicate | _Args],
+    var(_Args).
+
+
+logical_expression_to_list(milk(Atom), ['milk']).
+logical_expression_to_list(burger(Atom), ['burger']).
+
+
+
+
+
+
+logical_expression_to_list(with(A,B), List).
+logical_expression_to_list(recipe(A), List).
+logical_expression_to_list(want(A, B), List).
+
+logical_expression_to_list(and(Expression1, Expression2), List) :-
+    logical_expression_to_list(Expression1, List1),
+    logical_expression_to_list(Expression2, List2),
+    append(List1, List2, List).
+
+logical_expression_to_list(some(Variable, Expression), List) :-
+    logical_expression_to_list(Expression, List).
