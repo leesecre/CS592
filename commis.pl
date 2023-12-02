2a9c1b8f9ff6f677231e13d279431fe3403e19c7
Jaehwan Lee <59246292+leesecre@users.noreply.github.com>
2023-12-02 16:39:25 +0900

    merge commis.pl
    proto type complete

--- commis.pl
+++ commis.pl
@@ -1,7 +1,4 @@
-:- module(commis,[commis/0,
-                  commis/2]).
-
-:- use_module(readLine,[readLine/1]).
+:- module(commis,[commis/2]).
 
 :- use_module(comsemPredicates,[infix/0,
                                 prefix/0,
@@ -24,31 +21,6 @@
 :- [query].
 
 
-commis:-
-   write('Welcome to commis!'),
-   readLine(Sentence1),
-   commis(Sentence1,Sems1),
-   nth0(0,Sems1,Sem1),
-   extract_constants(Sem1,Ingredients),
-   %To print out the ingredients enable two lines below.
-   %write('The ingredients are: '),
-   %writeln(Ingredients),
-
-   write('Is there anything you cannot eat?'),
-   readLine(Sentence2),
-   commis(Sentence2,Sems2),
-   nth0(0,Sems2,Sem2),
-   extract_constants(Sem2,XIngredients),
-   %To print out the ingredients enable two lines below.
-   %write('The ingredients cannot eat are: '),
-   %writeln(XIngredients),
-   
-   write('Which recipe do you want?'),
-   readLine(Sentence3),
-   commis(Sentence3,Sems3),
-   nth0(0,Sems3,Sem3),
-   extract_constants(Sem3,Food),
-   write(Food).
 
 
    
--- recommender.pl
+++ recommender.pl
@@ -1,3 +1,29 @@
+/* Other modules */
+:- use_module(readLine,[readLine/1]).
+:- use_module(comsemPredicates,[infix/0,
+                                prefix/0,
+                                printRepresentations/1,
+				compose/3]).
+
+:- use_module(betaConversion,[betaConvert/2]).
+
+:- use_module(sentenceTestSuite,[sentence/2]).
+
+
+:- [englishGrammar].
+
+:- [englishLexicon].
+
+:- [semLexLambda].
+
+:- [semRulesLambda].
+
+:- [query].
+
+commis(Sentence,Sems):-
+	setof(Sem,t([sem:Sem],Sentence,[]),Sems).
+
+
 /* [GLOBAL DEFINE] */
 :- dynamic recipe/4.
 :- dynamic available_ingredient/1.
@@ -23,8 +49,16 @@ ask_for_substitute(Ingredient) :-
 /* [excluded_ingredients] */
 ask_for_excluded_ingredients :-
     format('Is there any you cannot eat? (type "none" when finished): '),
-    read_line_to_string(user_input, Ingredient),
-    (Ingredient \= "none" -> assertz(excluded_ingredient(Ingredient)), ask_for_excluded_ingredients; true).
+    read_line_to_string(user_input, IngredientString),
+    string_lower(IngredientString, Ingredient), % Convert to lowercase for consistency
+    (Ingredient \= "none" 
+    -> (atom_string(IngredientAtom, Ingredient), % Convert string to atom
+        assertz(excluded_ingredient(IngredientAtom)),
+        ask_for_excluded_ingredients)
+    ; true).
+
+list_excluded_ingredients(List) :-
+    findall(Ingredient, excluded_ingredient(Ingredient), List).
 
 /* [DATA STRUCTURE OPS] */
 equivalent_lists(List1, List2) :-
@@ -126,21 +160,24 @@ write_exclusions(_).
 % Predicate to recommend recipes based on available ingredients
 recommend_recipes() :-
     list_available_ingredients(AvailableIngredients),
-    findall(Recipe, (
+    findall((Recipe, Length), (
         recipe(Recipe, RecipeIngredients, _, _),
         subset(RecipeIngredients, AvailableIngredients),
         not_contains_excluded_ingredients(RecipeIngredients),
         length(RecipeIngredients, Length),
-        Length > 2  % To avoid abnormal data
+        Length > 4  % To avoid abnormal data
     ), CompleteMatches),
     findall((Recipe, MissingIngredients), (
         recipe(Recipe, RecipeIngredients, _, _),
         member_of_both(AvailableIngredients, RecipeIngredients),
+        \+ subset(RecipeIngredients, AvailableIngredients),
         \+ equivalent_lists(AvailableIngredients, RecipeIngredients),
         not_contains_excluded_ingredients(RecipeIngredients),
+        length(RecipeIngredients, Length),
+        Length > 4,
         find_missing_ingredients(AvailableIngredients, RecipeIngredients, MissingIngredients),
-        length(MissingIngredients, Length),
-        Length < 2
+        length(MissingIngredients, MissLength),
+        MissLength < 2
     ), PartialMatches),
     print_complete_matches(CompleteMatches),
     format('~n~c[31m[Recipes with some ingredients missing]~c[0m                 \n',[27,27]),
@@ -148,21 +185,24 @@ recommend_recipes() :-
 
 % Predicate to recommend recipes based on available ingredients
 recommend_recipes(AvailableIngredients) :-
-    findall(Recipe, (
+    findall((Recipe, Length), (
         recipe(Recipe, RecipeIngredients, _, _),
         subset(RecipeIngredients, AvailableIngredients),
         not_contains_excluded_ingredients(RecipeIngredients),
         length(RecipeIngredients, Length),
-        Length > 2  % To avoid abnormal data
+        Length > 4  % To avoid abnormal data
     ), CompleteMatches),
     findall((Recipe, MissingIngredients), (
         recipe(Recipe, RecipeIngredients, _, _),
         member_of_both(AvailableIngredients, RecipeIngredients),
+        \+ subset(RecipeIngredients, AvailableIngredients),
         \+ equivalent_lists(AvailableIngredients, RecipeIngredients),
         not_contains_excluded_ingredients(RecipeIngredients),
+        length(RecipeIngredients, Length),
+        Length > 4,
         find_missing_ingredients(AvailableIngredients, RecipeIngredients, MissingIngredients),
-        length(MissingIngredients, Length),
-        Length < 2
+        length(MissingIngredients, MissLength),
+        MissLength < 2
     ), PartialMatches),
     print_complete_matches(CompleteMatches),
     format('~n~c[31m[Recipes with some ingredients missing]~c[0m                 \n',[27,27]),
@@ -193,42 +233,55 @@ print_list_skip(List) :-
     length(List, Length),
     (   Length > 10
     ->  print_first_cut(List, 10)
-    ;   print_all(List) 
+    ;   print_list_row(List) 
     ),
-    write(']\n\n').
+    write(']\n').
 
 print_first_cut(_, 0) :- 
     write('...').
+
 print_first_cut([H|T], N) :-
     N > 0,
-    write(H), write(', '),
-    N1 is N - 1,
-    print_first_cut(T, N1).
+    write(H),
+    (   T == []  % Check if the tail is empty
+    ->  true     % Do nothing for the last element
+    ;   write(', '),  % Otherwise, print a comma
+        N1 is N - 1,
+        print_first_cut(T, N1)
+    ).
 
-print_all([]).
-print_all([H|T]) :-
-    write(H), write(', '),
-    print_all(T).
+print_list_row([]).
+print_list_row([H|T]) :-
+    write(H),
+    (   T == []  % Check if the tail is empty
+    ->  true     % Do nothing for the last element
+    ;   write(', '),  % Otherwise, print a comma
+        print_list_row(T)
+    ).
 
 print_list_bold([]).
 print_list_bold([Head|Tail]) :-
     format('* ~c[1m~w~c[0m\n', [27,Head,27]),
     print_list_bold(Tail).
 
+print_list_tuple_bold([]).
+print_list_tuple_bold([(H1, H2)|Tail]) :-
+    format('* ~c[1m~w~c[0m number of ingredients: ~w.\n', [27,H1,27, H2]),
+    print_list_tuple_bold(Tail).
+
 % Print complete matches
 print_complete_matches([]) :-
     format('~n[No recipes found with all ingredients available]           \n',[]).
-
 print_complete_matches(CompleteMatches) :-
     format('~n~c[34m[Recipes with all ingredients available]~c[0m                \n',[27,27]),
-    print_list_bold(CompleteMatches).
+    print_list_tuple_bold(CompleteMatches).
 
 print_partial_matches([], _) :-
     format('~n[No recipes found with partial ingredients ~c[34mavailable~c[0m]          \n',[27,27]).
 print_partial_matches(_, 0). % Stop printing after 10 matches
 print_partial_matches([(Recipe, MissingIngredients) | Rest], Counter) :-
     Counter > 0,
-    format('* ~c[1m~w~c[0m\nMissing ingredients: ', [27,Recipe,27]),
+    format('* ~c[1m~w~c[0m\n  -Missing ingredients: ', [27,Recipe,27]),
     print_list_skip(MissingIngredients),
     NewCounter is Counter - 1,
     print_partial_matches(Rest, NewCounter).
@@ -236,13 +289,21 @@ print_partial_matches([(Recipe, MissingIngredients) | Rest], Counter) :-
 
 
 % Predicate to print ingredients, highlighting missing ones in red
-print_ingredients([]).
+print_ingredients([]).  % Base case: do nothing for an empty list.
+print_ingredients([Ingredient]) :-
+    !,  % Cut to prevent backtracking
+    print_ingredient(Ingredient).
 print_ingredients([Ingredient|Rest]) :-
-    (   available_ingredient(Ingredient)
-    ->  format('~w, ', [Ingredient])
-    ;   print_red(Ingredient), format(', ')
-    ),
+    print_ingredient(Ingredient),
+    write(', '),
     print_ingredients(Rest).
+
+print_ingredient(Ingredient) :-
+    available_ingredient(Ingredient),
+    format('~w', [Ingredient]).
+print_ingredient(Ingredient) :-
+    \+ available_ingredient(Ingredient),
+    print_red(Ingredient).
 
 /* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 
@@ -268,44 +329,123 @@ find_missing_ingredients(Available, RecipeIngredients, Missing) :-
 
 
 /* USER INTERFACE */
+assert_avail_ingredients([]). % Base case: do nothing for an empty list.
+assert_avail_ingredients([H|T]) :-
+    assertz(available_ingredient(H)), % Assert the head of the list.
+    assert_avail_ingredients(T). % Recursively assert the rest of the list.
 
-add_available_ingredient:-
-    format('~c[1mcommis:~c[0m Okay, What do you have?\n',[27,27]),
-    read_line_to_string(user_input, Ingredient),
-    assertz(available_ingredient(Ingredient)),
+commis_add:-
+    format('~c[1mcommis:~c[0m Okay, What do you have? \n',[27,27]),
+    readLine(Sentence),
+    commis(Sentence,Sems),
+    nth0(0,Sems,Sem),
+    extract_constants(Sem,Ingredients),
+    assert_avail_ingredients(Ingredients),
     list_available_ingredients(AvailableIngredients),
-    format("Then now you have belows \n"),
-    print_list(AvailableIngredients).
+    format("~c[1mcommis:~c[0m Then now available ingredients are \n",[27,27]),
+    format('* ['),
+    print_list_row(AvailableIngredients),
+    format(']\n').
+
+
+delete_avail_ingredient(Ingredient) :-
+    retractall(available_ingredient(Ingredient)).
+
+delete_avail_ingredients([]).
+delete_avail_ingredients([H|T]) :-
+    retractall(available_ingredient(H)),
+    delete_avail_ingredients(T).
+
+commis_del:-
+    format('~c[1mcommis:~c[0m Which ingredient would you like to delete? \n',[27,27]),
+    readLine(Ingredient),
+    delete_avail_ingredient(Ingredient),
+    list_available_ingredients(AvailableIngredients),
+    format("~c[1mcommis:~c[0m Updated available ingredients are \n",[27,27]),
+    format('* ['),
+    print_list_row(AvailableIngredients),
+    format(']\n').
+
+assert_exc_ingredients([]). % Base case: do nothing for an empty list.
+assert_exc_ingredients([H|T]) :-
+    assertz(excluded_ingredient(H)), % Assert the head of the list.
+    assert_exc_ingredients(T). % Recursively assert the rest of the list.
+
+commis_exclude:-
+    format('~c[1mcommis:~c[0m Is there any you cannot eat? \n',[27,27]),
+    readLine(Sentence),
+    commis(Sentence,Sems),
+    nth0(0,Sems,Sem),
+    extract_constants(Sem,Ingredients),
+    assert_exc_ingredients(Ingredients),
+    list_excluded_ingredients(AvailableIngredients),
+    format("~c[1mcommis:~c[0m I recorded what you cannot eat here. \n",[27,27]),
+    format('* ['),
+    print_list_row(AvailableIngredients),
+    format(']\n').
+
+
+commis_sub:-
+    format('TODO').
+
+commis_ask:-
+    recommend_recipes.
+
+commis_detail:-
+    format('~c[1mcommis:~c[0m which recipe you want to know? \n',[27,27]),
+    readLine(Sentence),
+    commis(Sentence,Sems),
+    nth0(0,Sems,Sem),
+    extract_constants(Sem,Recipe),
+    recipe_details(Recipe).
 
 hey_commis :-
     format('~c[1mcommis:~c[0m Hello, do you need help? :)\n',[27,27]),
-    format('    1.  (type "add") want to add available ingredients.\n'),
-    format('    2.  (type "exc") tell me ingredients you can\'t eat.\n'),
-    format('    3.  (type "tea") teach me substitute for ingredients.\n'),
+    format('    1.  (type "add") tell commis adding available ingredients.\n'),
+    format('    2.  (type "del") tell commis deleting available ingredients.\n'),
+    format('    3.  (type "exc") tell commis ingredients you can\'t eat.\n'),
+    format('    4.  (type "sub") tell commis substitute for some ingredients.\n'),
+    format('    5.  (type "ask") ask commis possible recipe for now.\n'),
+    format('    6.  (type "det") ask commis for the details of a recipe.\n'),
     read_line_to_string(user_input, Command),
-    (Command = "add" -> add_available_ingredient, !;
-     Command = "exc" -> format('hi'), !;
-     Command = "tea" -> format('hi'), !;
-     format("I cannot understand what yor say"), false).
+    (Command = "add" -> commis_add, !;
+     Command = "del" -> commis_del, !;
+     Command = "exc" -> commis_exclude, !;
+     Command = "sub" -> commis_sub, !;
+     Command = "ask" -> commis_ask, !;
+     Command = "det" -> commis_detail, !;
+     format("~c[1mcommis:~c[0m ~c[31m(ERROR) I cannot understand what yor say.~c[0m\n",[27,27,27,27]), false).
 
 /*========================================================================
    Start Interface.
 ========================================================================*/
 
-info:-
-   format('~n>~c[1m ------------------------------------------------------------------- ~c[0m<',[27, 27]),
-   format('~n>~c[1m Commis: food recommdender by Jaehwan Lee and Insun Baek             ~c[0m<',[27, 27]),
-   format('~n>                                                                     <',[]),
-   format('~n> ?- ~c[32mload_all.~c[0m      - Load recipes and user data from storage         <',[27,27]),
-   format('~n> ?- ~c[32msave_all.~c[0m      - Save recipes and user data to storage           <',[27,27]),
-   format('~n> ?- ~c[32mhey_commis.~c[0m    - Ask commis, interface to communicate            <',[27,27]),
-   format('~n>                                                                     <',[]),
-   format('~n> (Example)                                                           <',[]),
-   format('~n> ?- load_all.                                                    <',[]),
-   format('~n> ?- recommend_recipes([\'sugar\', \'salt\', \'pork\', \'onion\']).           <',[]),
-   format('~n> ?- recipe_details(\'pork strips\').                                   <',[]),
-   format('~n>                                                                     <',[]),
-   format('~n>~c[1m ------------------------------------------------------------------- ~c[0m<',[27,27]),
+info:- 
+   format('~n>~c[1m ----------------------------------------------------------------------------- ~c[0m<',[27, 27]),
+    format('~n'),
+    format('       .--,--.~n'),
+    format('       `.  ,.''~n'),
+    format('        |___|~n'),
+    format('        :o o:    O    ~c[1mCOMMIS~c[0m, your food recommdender!~n',[27, 27]),
+    format('       _`.v.''_   |    by Jaehwan Lee and Insun Baek ~n'),
+    format('     /''   ^    `W=)      version 1.0 ~n'),
+    format('   .''  _______ ''-|~n'),
+    format('   `(<=|     |= /''~n'),
+    format('       |     |~n'),
+    format('       |_____|~n'),
+    format('--------=====-------------'),
+   format('~n>                                                                               <',[]),
+   format('~n> ?- ~c[32mload_all.~c[0m      - Load recipes and user data from storage                   <',[27,27]),
+   format('~n> ?- ~c[32msave_all.~c[0m      - Save recipes and user data to storage                     <',[27,27]),
+   format('~n> ?- ~c[32mhey_commis.~c[0m    - Ask commis, interface to communicate                      <',[27,27]),
+   format('~n> ?-      ~c[32m(add)~c[0m     - add available ingredients                                 <',[27,27]),
+   format('~n> ?-      ~c[32m(del)~c[0m     - delete available ingredients                              <',[27,27]),
+   format('~n> ?-      ~c[32m(exc)~c[0m     - exclude some ingredients                                  <',[27,27]),
+   format('~n> ?-      ~c[32m(sub)~c[0m     - tell substitute of ingredients                            <',[27,27]),
+   format('~n> ?-      ~c[32m(ask)~c[0m     - ask possible recipe                                       <',[27,27]),
+   format('~n> ?-      ~c[32m(det)~c[0m     - ask recipe details                                        <',[27,27]),
+   format('~n>                                                                               <',[]),
+   format('~n>~c[1m ----------------------------------------------------------------------------- ~c[0m<',[27,27]),
    format('~n~n',[]).
 
 :- initialization(info).
